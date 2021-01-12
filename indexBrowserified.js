1b8287ef6efa3c4c3381b4f1813d45f8061e0a87
Avi Garg <a7vi1912@gmail.com>
2021-01-12 09:35:30 +0000

    ID change

--- indexBrowserified.js
+++ indexBrowserified.js
@@ -3,7 +3,7 @@ const SDK = require("furioos-sdk");
 const Player = SDK.Player;
 
 // CONF
-const player = new Player('133.311', 'furioos_container', {
+const player = new Player('221.769', 'furioos_container', {
   whiteLabel: true,
   hideTitle: true,
   hideToolbar: false,
